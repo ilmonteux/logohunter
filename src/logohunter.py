052060e155c717eace54137769c6fea61d254935
angelo <anmonteux@gmail.com>
2019-02-11 09:41:45 +0000

    updated train pipeline

--- src/logohunter.py
+++ src/logohunter.py
@@ -1,6 +1,6 @@
 #
 # MASTER SCRIPT TO DETECT LOGOS IN IMAGE AND FIND MATCHES
-# for each image, pass logos, compute features, see if logo passes cutoff
+# for each image find and pass logos, compute features, see if logo passes cutoff, save image
 #
 
 import sys, os
@@ -9,26 +9,16 @@ import cv2
 import numpy as np
 
 import argparse
-import readline
-readline.parse_and_bind("tab: complete")
 
 from keras_yolo3.yolo import YOLO
-from utils import contents_of_bbox, load_features, pad_image, bbox_colors
-from similarity import features_from_image, similarity_cutoff, similar_matches, draw_matches
+from utils import contents_of_bbox, load_features, bbox_colors, parse_input, chain_preprocess, load_extractor_model
+from similarity import features_from_image, similarity_cutoff, load_brands_compute_cutoffs, similar_matches, draw_matches
 
 from timeit import default_timer as timer
 
 input_shape = (299,299,3)
-
-def load_extractor_model():
-    from keras.applications.inception_v3 import InceptionV3
-    from keras.applications.inception_v3 import preprocess_input
-    model = InceptionV3(weights='imagenet', include_top=False)
-
-    return model, preprocess_input
-
-def preprocess(img, input_shape, preprocess_func):
-    return preprocess_func(pad_image(img, input_shape))
+sim_threshold = 0.95
+save_img_logo, save_img_match = True, True
 
 def detect_logo(yolo, img_path, save_img = False, save_img_path='./', postfix=''):
     """
@@ -57,14 +47,33 @@ def detect_logo(yolo, img_path, save_img = False, save_img_path='./', postfix=''
 
     return prediction, r_image
 
+def match_logo(img_path, prediction, model_preproc, input_features_cdf_cutoff_labels,
+               save_img = False, save_img_path='./'):
 
-yolo = YOLO(**{"model_path": 'keras_yolo3/logs/003/trained_weights_final.h5',
-        "anchors_path": 'keras_yolo3/model_data/yolo_anchors.txt',
-        "classes_path": 'keras_yolo3/data_classes.txt',
-        "score" : 0.1,
-        "model_image_size" : (416, 416),
-            }
-           )
+    model, my_preprocess = model_preproc
+    feat_input, sim_cutoff, bins, cdf_list, input_labels = input_features_cdf_cutoff_labels
+    img_test = cv2.imread(img_path)
+    candidates = contents_of_bbox(img_test, prediction)
+    features_cand = features_from_image(candidates, model, my_preprocess)
+    matches, cos_sim = similar_matches(feat_input, features_cand, sim_cutoff, bins, cdf_list)
+
+    outtxt = img_path
+    for idx in matches:
+        bb = prediction[idx]
+        label = input_labels[matches[idx][0]]
+        print('Logo #{} - {} {} - classified as {} {:.2f}'.format(idx,
+          tuple(bb[:2]), tuple(bb[2:4]), label, matches[idx][1]))
+
+        outtxt += ' {},{},{},{},{},{:.2f},{:.3f}'.format(*bb[:4], label,bb[-1], matches[idx][1])
+    outtxt += '\n'
+
+    new_img = draw_matches(img_test, input_labels, prediction, matches)
+
+    if save_img:
+        cv2.imwrite(os.path.join(save_img_path, os.path.basename(img_path)), new_img)
+
+    return outtxt
+
 
 def test():
 
@@ -76,7 +85,7 @@ def test():
 
     ## load inception model
     model, preprocess_input = load_extractor_model()
-    my_preprocess = lambda x: preprocess(x, input_shape, preprocess_input)
+    my_preprocess = lambda x: chain_preprocess(x, input_shape, preprocess_input)
 
     ## load sample images of logos to test against
     input_paths = ['test_batman.jpg', 'test_robin.png', 'test_lexus.png', 'test_champions.jpg',
@@ -127,8 +136,8 @@ def test():
 
 
 
-test()
-sys.exit()
+# test()
+# sys.exit()
 
 FLAGS = None
 
@@ -145,38 +154,53 @@ if __name__ == '__main__':
     )
 
     parser.add_argument(
-        "--input_images", nargs='?', type=str, required=False, default='input',
-        help = "path to image or video to find logos in"
+        "--input_images", type=str, default='input',
+        help = "path to image directory or video to find logos in"
     )
 
     parser.add_argument(
-        "--input_brands", nargs='?', type=str, required=True,
-        help = "path to directory with all brand logos to find in input_images"
+        "--input_brands", type=str, default='input',
+        help = "path to directory with all brand logos to find in input images"
     )
 
     parser.add_argument(
-        '--batch', type=str, required=False,
-        help='Image detection mode for each file specified in input txt, will ignore all positional arguments'
+        '--batch', default=False, action="store_true",
+        help='Image detection mode for each file specified in input txt'
     )
 
     parser.add_argument(
-        "--output", nargs='?', type=str, default="../data/output/",
+        "--output", type=str, default="../data/output/",
         help = "output path: either directory for single/batch image, or filename for video"
     )
 
     parser.add_argument(
-        '--model', type=str, dest='model_path', default = '../build/yolo_weights_logos.h5',
-        help='path to model weight file'
+        '--yolo_model', type=str, dest='model_path', default = 'keras_yolo3/yolo_weights_logos.h5',
+        help='path to YOLO model weight file'
     )
 
     parser.add_argument(
-        '--gpu_num', type=int, default = 2,
-        help='Number of GPU to use '
+        '--anchors', type=str, dest='anchors_path', default = 'keras_yolo3/model_data/yolo_anchors.txt',
+        help='path to YOLO anchors'
+    )
+
+    parser.add_argument(
+        '--classes', type=str, dest='classes_path', default = 'keras_yolo3/data_classes.txt',
+        help='path to YOLO class specifications'
+    )
+
+    parser.add_argument(
+        '--gpu_num', type=int, default = 1,
+        help='Number of GPU to use'
     )
 
     parser.add_argument(
         '--confidence', type=float, dest = 'score', default = 0.1,
         help='YOLO confidence threshold above which to show predictions'
+    )
+
+    parser.add_argument(
+        '--features', type=str, dest='features', default = 'inception_logo_features.hdf5',
+        help='path to LogosInTheWild logos features extracted by InceptionV3'
     )
 
     parser.add_argument(
@@ -186,52 +210,100 @@ if __name__ == '__main__':
 
     FLAGS = parser.parse_args()
 
+
     if FLAGS.image:
         """
         Image detection mode, either prompt user input or was passed as argument
         """
         print("Image detection mode")
 
-        yolo = YOLO(**vars(FLAGS))
-        if FLAGS.input_images == 'input':
-            while True:
-                FLAGS.input = input('Input image filename (q to quit):')
-                if FLAGS.input in ['q','quit']:
-                    yolo.close_session()
-                    exit()
+        save_to_txt = False
 
-                img = FLAGS.input
-                prediction, r_image = detect_logo(yolo, img, save_img = True, save_img_path=FLAGS.output)
-                if prediction is None:
-                    continue
+        if FLAGS.input_brands == 'input':
+            print('Input logos to search for in images: (file-by-file or entire directory)')
 
+            FLAGS.input_brands = parse_input()
+
+        elif os.path.isdir(FLAGS.input_brands):
+            FLAGS.input_brands = [ os.path.abspath(os.path.join(FLAGS.input_brands, f)) for f in os.listdir(FLAGS.input_brands) if f.endswith(('.jpg', '.png')) ]
         else:
-            img = FLAGS.input
-            prediction, r_image = detect_logo(yolo, img, save_img = True, save_img_path=FLAGS.output)
+            exit('Error: path not found:', FLAGS.input_brands)
 
-        yolo.close_session()
 
-    elif 'batch' in FLAGS:
-        print("Batch image detection mode: reading "+FLAGS.batch)
+        if FLAGS.batch and FLAGS.input_images.endswith('.txt'):
+            print("Batch image detection mode: reading "+FLAGS.input_images)
+            with open(FLAGS.input_images, 'r') as file:
+                file_list = [line.split(' ')[0] for line in file.read().splitlines()]
+            FLAGS.input_images = [os.path.abspath(f) for f in file_list]
 
-        with open(FLAGS.batch, 'r') as file:
-            file_list = [line.split(' ')[0] for line in file.read().splitlines()]
-        out_txtfile = os.path.join(FLAGS.output, 'data_pred.txt')
-        txtfile = open(out_txtfile,'w')
+            output_txt = FLAGS.input_images.split('.txt')+'_pred.txt'
+            save_to_txt = True
 
-        yolo = YOLO(**vars(FLAGS))
+        elif FLAGS.input_images == 'input':
+            print('Input images to be scanned for logos: (file-by-file or entire directory)')
+            FLAGS.input_images = parse_input()
 
-        for img in file_list[:10]:
-            prediction, r_image = detect_logo(yolo, img, save_img = True, save_img_path='output')
+        elif os.path.isdir(FLAGS.input_images):
+            FLAGS.input_images = [ os.path.abspath(os.path.join(FLAGS.input_images, f)) for f in os.listdir(FLAGS.input_images) if f.endswith(('.jpg', '.png')) ]
+        else:
+            exit('Error: path not found:', FLAGS.input_images)
 
-            txtfile.write(img+' ')
-            for pred in prediction:
-                txtfile.write(','.join([str(p) for p in pred])+' ')
-            txtfile.write('\n')
 
-        txtfile.close()
-        yolo.close_session()
-    # elif "input" in FLAGS:
-    #     detect_video(YOLO(**vars(FLAGS)), FLAGS.input, FLAGS.output)
+        print('Found {} input brands: {}...'.format(len(FLAGS.input_brands), [ os.path.basename(f) for f in FLAGS.input_brands[:5]]))
+        print('Found {} input images: {}...'.format(len(FLAGS.input_images), [ os.path.basename(f) for f in FLAGS.input_images[:5]]))
+
+        output_path = FLAGS.output
+        if not os.path.exists(output_path):
+            os.makedirs(output_path)
+
+        # define YOLO logo detector
+        yolo = YOLO(**{"model_path": FLAGS.model_path,
+                    "anchors_path": FLAGS.anchors_path,
+                    "classes_path": FLAGS.classes_path,
+                    "score" : FLAGS.score,
+                    "gpu_num" : FLAGS.gpu_num,
+                    "model_image_size" : (416, 416),
+                    }
+                   )
+
+
+        input_paths = sorted(FLAGS.input_brands)
+        input_labels = [ os.path.basename(s).split('test_')[-1].split('.')[0] for s in input_paths]
+
+        ## load pre-processed features database
+        brand_map, features = load_features(FLAGS.features)
+
+        ## load inception model
+        model, preprocess_input = load_extractor_model()
+        my_preprocess = lambda x: chain_preprocess(x, input_shape, preprocess_input)
+
+        # compute cosine similarity between input brand images and all LogosInTheWild logos
+        ( img_input, feat_input, sim_cutoff, (bins, cdf_list)
+        ) = load_brands_compute_cutoffs(input_paths, (model, my_preprocess), features, sim_threshold)
+
+        start = timer()
+        # cycle trough input images, look for logos and then match them against inputs
+        text_out = ''
+        for i, img_path in enumerate(FLAGS.input_images):
+            prediction, r_image = detect_logo(yolo, img_path, save_img = save_img_logo,
+                                              save_img_path = FLAGS.output,
+                                              postfix='_logo')
+
+            text = match_logo(img_path, prediction, (model, my_preprocess),
+                    (feat_input, sim_cutoff, bins, cdf_list, input_labels),
+                    save_img = save_img_match, save_img_path=FLAGS.output)
+            text_out += text
+
+        if save_to_txt:
+            with open(output_txt,'w') as txtfile:
+                txtfile.write(text_out)
+
+        end = timer()
+        print('Processed {} images in {:.1f}sec - {:.1f}FPS'.format(
+             len(FLAGS.input_images), end-start, len(FLAGS.input_images)/(end-start)
+             ))
+
+    # video mode
+    # elif FLAGS.video:
     else:
-        print("Must specify at least video_input_path.  See usage with --help.")
+        print("Must specify either --image or --video.  See usage with --help.")
